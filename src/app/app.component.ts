2eddec6b3ffc54e1fabbae13807d0d56637e55ed
faysaljafry <faysaljafryofficial@gmail.com>
2021-10-04 15:51:18 +0500

    Directives uptill ngSwitch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Directives';
-  numbers = [1, 2, 3, 4, 5, 6, 7];
+  //numbers = [1, 2, 3, 4, 5, 6, 7];
+  oddNumbers = [1, 3, 5, 7];
+  evenNumbers = [2, 4, 6];
   onlyOdd = false;
+  myValue = 5;
 }
