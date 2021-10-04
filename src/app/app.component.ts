50f4004e3f6491ace8601007bd0b5be6a7a61924
faysaljafry <faysaljafryofficial@gmail.com>
2021-10-04 12:13:00 +0500

    Setting up app Env

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Directives';
+  numbers = [1, 2, 3, 4, 5, 6, 7];
+  onlyOdd = false;
 }
